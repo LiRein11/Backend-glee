37142205cc10167ab969c2db3237f1654158296a
Stepanov Sergey <sergey11200011@mail.ru>
2023-08-23 17:30:14 +0500

    new deploy

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
 });
